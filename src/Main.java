e6a113ec1253f27d62f64d3d0eb51b8ef376a405
VinishaGupta <vinishasgupta@gmail.com>
2025-03-24 03:14:09 +0530

    a1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year=in.nextInt();
+        in.close();
+        if(checkLeap(year)){
+            System.out.println(year+ " is a leap year.");
+        } else{
+            System.out.println(year+" is not a leap year");
+        }
+
+    }
+
+    public static boolean checkLeap(int year){
+        return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+    }
+}
